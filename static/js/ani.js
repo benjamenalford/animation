0b4dde6d48b9107b0a04d4f812c135fa8ce58155
Benjamen Alford <benjamen.alford@gmail.com>
2024-07-02 22:21:06 -0500

    update

--- static/js/ani.js
+++ static/js/ani.js
@@ -2,11 +2,12 @@ console.log('js')
 let gridContainer = d3.select("#grid");
 const GRID_SIZE = 8;
 let gridArray = [];
-
+let animationActive = 0
+let timer = 1
 //lets make a font!
 let font = {}
-font.h = [17, 18, 19, 20, 21, 22, 28, 36, 41, 42, 43, 44, 45, 46];
-font.i = [26, 27, 28, 29, 30];
+font.h = [17, 18, 19, 20, 21, 22, 28, 36, 41, 42, 43, 44, 45, 46]
+font.i = [27, 28, 29, 30]
 font.dot = [46]
 
 
@@ -33,41 +34,33 @@ gridArray.forEach((square) => {
         .style('top', (y * 50) + 'px')
         .on('click', (e) => {
             console.log(d3.select(e.target).attr('id'))
-        });
+        }).text(square.id);
 });
 
 function drawChar(char = '') {
-    if (char === '.') {
+    console.log(char)
+    if (char == '.') {
         char = 'dot'
     }
     gridArray.forEach((square, i) => {
         if (font[char].includes(i)) {
             d3.select(`#${square.id}`).attr('class', 'grid-square grid-square-on');
         } else {
-            d3.select(`#${square.id}`).attr('class', 'grid-square')
-                .style('background-color', randomBGColor());
+            d3.select(`#${square.id}`).attr('class', 'grid-square grid-square-off');
         }
+
     });
+
 }
 
-function drawText(text = 'hi.') {
+function drawText(text = '') {
     text.split('').forEach((char, i) => {
         setTimeout(() => {
             drawChar(char);
-        }, (i * 3000));
+        }, (3000 * i));
     })
-    gridArray.forEach((square, i) => {
-        d3.select(`#${square.id}`).attr('class', 'grid-square')
-            .style('background-color', randomBGColor());
-        d3.select(`#${square.id}`).on('animationend', function () {
-            //letter drawn
-            // d3.select(`#${square.id}`).style('background-color', randomBGColor());
-            //console.log('drawn')
-        });
-    });
 
 }
-drawText('hi.')
 
 function randomBGColor() {
     let r = Math.floor(128 - Math.random() * 20)
@@ -75,3 +68,21 @@ function randomBGColor() {
     let b = Math.floor(212 - Math.random() * 20)
     return (`rgb(${r}, ${g}, ${b})`)
 }
+function init() {
+    gridArray.forEach((square, i) => {
+        d3.select(`#${square.id}`).attr('class', 'grid-square')
+            .style('background-color', randomBGColor());
+        d3.select(`#${square.id}`).on('animationend', function () {
+            //letter drawn
+            animationActive--;
+
+        });
+        d3.select(`#${square.id}`).on('animationstart', function () {
+            //letter starting
+            animationActive++;
+        });
+
+    });
+}
+init()
+drawText('hi.'.repeat(10))
